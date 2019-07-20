42d55c93fd3fc3411d1435739c3df566e3e924bb
Beatriz <bccp02@gmail.com>
2019-07-20 01:36:31 -0300

    Teste001

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let Botaoazul = document.querySelector('#azul');
+let Botaovermelho = document.querySelector('#vermelho');
+let Botaoverde = document.querySelector('#verde');
+let Botaoamarelo = document.querySelector('#amarelo');
+
+let botoes = [Botaoazul, Botaovermelho, Botaoverde, Botaoamarelo]
+
+
+
