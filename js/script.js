5beb1b77dfbea116e0777dd4314bb427d44bb9c0
Beatriz <bccp02@gmail.com>
2019-07-20 02:48:51 -0300

    criar indice aleatorio

--- js/script.js
+++ js/script.js
@@ -5,5 +5,18 @@ let Botaoamarelo = document.querySelector('#amarelo');
 
 let botoes = [Botaoazul, Botaovermelho, Botaoverde, Botaoamarelo]
 
-
-
+function embaralhar(botoes) {
+    let valor_temporario;
+    let indice_aleatorio;
+  
+    for (let i = botoes.length - 1; i !== 0; i--) {
+      indice_aleatorio = Math.floor(Math.random() * i);
+  
+      valor_temporario = botoes[i];
+      botoes[i] = botoes[indice_aleatorio];
+      botoes[indice_aleatorio] = valor_temporario;
+    }
+    return botoes;
+  };
+  
+console.log(embaralhar(botoes));
