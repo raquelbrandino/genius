4270466ed0fe41ec2ae30f602c1b282f2dbf69ac
Beatriz <bccp02@gmail.com>
2019-07-20 19:46:08 -0300

    recomeçar

--- js/script.js
+++ js/script.js
@@ -3,20 +3,9 @@ let Botaovermelho = document.querySelector('#vermelho');
 let Botaoverde = document.querySelector('#verde');
 let Botaoamarelo = document.querySelector('#amarelo');
 
-let botoes = [Botaoazul, Botaovermelho, Botaoverde, Botaoamarelo]
+let TodosBotoes = [Botaoazul, Botaovermelho, Botaoverde, Botaoamarelo]
 
-function embaralhar(botoes) {
-    let valor_temporario;
-    let indice_aleatorio;
+function(TodosBotoes){
   
-    for (let i = botoes.length - 1; i !== 0; i--) {
-      indice_aleatorio = Math.floor(Math.random() * i);
-  
-      valor_temporario = botoes[i];
-      botoes[i] = botoes[indice_aleatorio];
-      botoes[indice_aleatorio] = valor_temporario;
-    }
-    return botoes;
-  };
-  
-console.log(embaralhar(botoes));
+
+}
