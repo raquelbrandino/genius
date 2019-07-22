fc210fdaca1f9179a0ef9263c42700170f71c5c4
emilyduraes <emilyduraes@yahoo.com.br>
2019-07-21 22:21:53 -0300

    adicionar contador de tempo

--- js/script.js
+++ js/script.js
@@ -75,11 +75,24 @@ let iniciar = () => {
   gerarOrdem();
 }
 
+function contadorTempo(op) {
+	let s = 1;
+	let m = 0;
+  let h = 0;
+  
+	intervalo = window.setInterval(function() {
+		if (s == 60) { m++; s = 0; }
+		if (m == 60) { h++; s = 0; m = 0; }
+		if (h < 10) document.getElementById("hora").innerHTML = "0" + h + "h"; else document.getElementById("hora").innerHTML = h + "h";
+		if (s < 10) document.getElementById("segundo").innerHTML = "0" + s + "s"; else document.getElementById("segundo").innerHTML = s + "s";
+		if (m < 10) document.getElementById("minuto").innerHTML = "0" + m + "m"; else document.getElementById("minuto").innerHTML = m + "m";		
+		s++;
+	},1000);
+}
+
+window.onload = contadorTempo();
 verde.onclick = () => clicou(0);
 vermelho.onclick = () => clicou(1);
 amarelo.onclick = () => clicou(2);
 azul.onclick = () => clicou(3);
 iniciar();
-
-
-
